b0da6e3979c94d94c3a9c91859dfb793bf669de9
Doug Anarino <form57@syntropo.com>
2014-11-26 21:46:57 -0500

    Fix method calls in pathutils.php

--- app/php/include/pathutils.php
+++ app/php/include/pathutils.php
@@ -2,7 +2,7 @@
 
 if (! function_exists('path_add_slashes')) {
 	function path_add_slashes($s) {
-		return add_slash_start(add_slash_end($s));
+		return path_add_slash_start(path_add_slash_end($s));
 	}
 }
 if (! function_exists('path_add_slash_end')) {
@@ -24,13 +24,13 @@ if (! function_exists('path_concat')) {
 		if (empty($s1)) $s1 = '';
 		if (empty($s2)) $s2 = '';
 		if (empty($s1) || empty($s2)) $s1 .= $s2;
-		else $s1 = add_slash_end($s1) . strip_slash_start($s2);
+		else $s1 = path_add_slash_end($s1) . path_strip_slash_start($s2);
 		return $s1;
 	}
 }
 if (! function_exists('path_strip_slashes')) {
 	function path_strip_slashes($s) {
-		return strip_slash_start(strip_slash_end(s));
+		return path_strip_slash_start(path_strip_slash_end(s));
 	}
 }
 
