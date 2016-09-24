0f2b8f4514625e510056a53638e815a665ab9f78
patrick henry <bitbucket@patrickhenry.co.uk>
2016-09-24 00:17:06 -0400

    no jquery mic and camera no storage

--- app/app/js/ui.js
+++ app/app/js/ui.js
@@ -1,5 +1,20 @@
-   function toggle_visibility(){
+function toggle_visibilityCapture(){
 
  document.getElementById('CapturePanel').classList.toggle('hidden');
  document.getElementById('CapturePanel').classList.toggle('visible')
+}
+
+
+
+function toggle_visibilityInspector(){
+
+ document.getElementById('InspectorPanel').classList.toggle('hidden');
+ document.getElementById('InspectorPanel').classList.toggle('visible')
+}
+
+
+function toggle_visibilityImport(){
+
+ document.getElementById('ContentPanel').classList.toggle('hidden');
+ document.getElementById('ContentPanel').classList.toggle('visible')
 }
