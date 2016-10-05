213c773c5344ac8f10c53db14b81a9e71ce37f0f
patrick henry <bitbucket@patrickhenry.co.uk>
2016-10-05 03:19:49 -0400

    filters and UI and fixs

--- app/app/js/ui.js
+++ app/app/js/ui.js
@@ -18,3 +18,20 @@ function toggle_visibilityImport(){
  document.getElementById('ContentPanel').classList.toggle('hidden');
  document.getElementById('ContentPanel').classList.toggle('visible')
 }
+
+
+//Filters
+var idx = 0;
+var filters = ['grayscale', 'sepia', 'blur', 'brightness', 'contrast', 'hue-rotate',
+               'hue-rotate2', 'hue-rotate3', 'saturate', 'invert', ''];
+
+function changeFilter(e) {
+  var el = e.target;
+  el.className = '';
+  var effect = filters[idx++ % filters.length]; // loop through filters.
+  if (effect) {
+    el.classList.add(effect);
+  }
+}
+
+document.querySelector('video').addEventListener('click', changeFilter, false);
